25e7d51d62b58388ec848a9df893bc2e4b69e6a0
0alif <alifhossain003@gmail.com>
2021-10-29 03:47:24 +0600

    first commit

---
+++ src/Pages/Login/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Login.css';
+
+const Login = () => {
+    return (
+        <div>
+            <h2>Please login</h2>
+            <h5>Developer is sleeping...</h5>
+        </div>
+    );
+};
+
+export default Login;
