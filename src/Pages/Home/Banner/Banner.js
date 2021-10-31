c7a00ebdbced9b2b225410e2b625e6a3b0a63f43
0alif <alifhossain003@gmail.com>
2021-10-31 18:57:09 +0600

    banana

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -6,19 +6,45 @@ const Banner = () => {
   return (
     <div>
       <div className="hero-img">
-        <img className="img-fluid" src={hero} alt="" />
-      </div>
-      <div className="hero-text">
-        <h1>Your Favorite Food, on the way!</h1>
-        <p>
-          The answer is easy. Food is universal. Everybody has to eat! It’s as
-          simple as that. So people want to find tasty, healthy food, the best
-          places to eat, etc. It’s no wonder that everybody’s talking about
-          food.
-        </p>
+        <div className="hero-text">
+          <h1>Your Favorite Food, on the way!</h1>
+          <p>
+            The answer is easy. Food is universal. Everybody has to eat! It’s as
+            simple as that. So people want to find tasty, healthy food, the best
+            places to eat, etc. It’s no wonder that everybody’s talking about
+            food.
+          </p>
+          <div class="d-flex">
+            <input
+              class="form-control me-2 w-75"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button class="btn submit-btn" type="submit">
+              Search
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
 };
 
 export default Banner;
+
+{
+  /* <img className="img-fluid" src={hero} alt="" /> */
+}
+
+{
+  /* <div className="hero-text">
+          <h1>Your Favorite Food, on the way!</h1>
+          <p>
+            The answer is easy. Food is universal. Everybody has to eat! It’s as
+            simple as that. So people want to find tasty, healthy food, the best
+            places to eat, etc. It’s no wonder that everybody’s talking about
+            food.
+          </p>
+        </div> */
+}
--- src/Pages/Home/Discover/Discover.js
+++ src/Pages/Home/Discover/Discover.js
@@ -1,25 +1,45 @@
-import React from 'react';
-import './Discover.css';
+import React from "react";
+import "./Discover.css";
 
 const Discover = () => {
-    return (
-        <div>
-            <div className="row m-0 mt-5 container">
-                <div className="col-sm-12 col-md-6 discover-text">
-                    <h1>Find Favorites and Discover New Ones</h1>
-                    <p>No matter where you are and what you crave, our riders are ready to deliver from a wide range of restaurants and cuisines. Eat restaurant food at the comfort of your home!</p>
-                    <h3>Download the App and Place Your Order</h3>
-                    <p>Nothing makes food tastier than the topping of a good deal. Our continuous promotions and discounts will keep your lips smacking, always.</p>
-                    <img className="my-4 me-3" src="https://puzzlerbox.com/templatekits-pro/food-delivery/wp-content/uploads/sites/3/2021/04/appstore_dark.png" alt="" />
-                    <img className="my-4 me-3" src="https://puzzlerbox.com/templatekits-pro/food-delivery/wp-content/uploads/sites/3/2021/04/googlestore_dark.png" alt="" />
-                </div>
-                <div className="col-sm-12 col-md-6">
-                    <img className="img-fluid" src="https://puzzlerbox.com/templatekits-pro/food-delivery/wp-content/uploads/sites/3/2021/04/app.png" alt="" />
-                </div>
-                <hr className="mt-4" style={{color:"magenta"}} />
-            </div>
+  return (
+    <div>
+      <div className="row m-0 mt-5 container">
+        <div className="col-sm-12 col-md-6 discover-text">
+          <h1>Find Favorites and Discover New Ones</h1>
+          <p>
+            No matter where you are and what you crave, our riders are ready to
+            deliver from a wide range of restaurants and cuisines. Eat
+            restaurant food at the comfort of your home!
+          </p>
+          <h3>Download the App and Place Your Order</h3>
+          <p>
+            Nothing makes food tastier than the topping of a good deal. Our
+            continuous promotions and discounts will keep your lips smacking,
+            always.
+          </p>
+          <img
+            className="my-4 me-3"
+            src="https://puzzlerbox.com/templatekits-pro/food-delivery/wp-content/uploads/sites/3/2021/04/appstore_dark.png"
+            alt=""
+          />
+          <img
+            className="my-4 me-3"
+            src="https://puzzlerbox.com/templatekits-pro/food-delivery/wp-content/uploads/sites/3/2021/04/googlestore_dark.png"
+            alt=""
+          />
         </div>
-    );
+        <div className="col-sm-12 col-md-6">
+          <img
+            className="img-fluid ps-5"
+            src="https://puzzlerbox.com/templatekits-pro/food-delivery/wp-content/uploads/sites/3/2021/04/app.png"
+            alt=""
+          />
+        </div>
+        <hr className="mt-4" style={{ color: "magenta" }} />
+      </div>
+    </div>
+  );
 };
 
 export default Discover;
